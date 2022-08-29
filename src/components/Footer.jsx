8986990a02c822bd22bb0c18dd9da656e0ef9747
SidouGmr7 <goumirisidali@hotmail.com>
2022-08-29 11:56:40 +0100

    up

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,20 +5,42 @@ import SocialItem from '../components/SocialItem'
 function Footer() {
   return (
     <div className='py-5 border-t-3/2'>
-     <div className='flex gap-2 justify-center mt-4 md:gap-8'>
-        <SocialItem name='Github' href='https://github.com/SidouGmr7/' Color='gray-800'/>
-        <SocialItem name='Facebook' href='https://www.facebook.com/Sidali.Gmr/' Color='blue-600'/>
-        <SocialItem name='Linkedin' href='https://www.linkedin.com/in/sidali-gmr-4bbba21a4/' Color='blue-500'/>
-        <SocialItem name='Email' href='https://mailto:goumirisidali@hotmail.com' Color='teal-500'/>
-        <SocialItem name='Instagram' href=' https://www.instagram.com/sidou_gmr/' Color='orange-600'/>
-        <SocialItem name='Messanger' href=' https://www.instagram.com/sidou_gmr/' Color='blue-500'/>
+      <div className='flex gap-2 justify-center mt-4 md:gap-8'>
+        <SocialItem
+          name='Github'
+          href='https://github.com/SidouGmr7/'
+          Color='gray-800'
+        />
+        <SocialItem
+          name='Facebook'
+          href='https://www.facebook.com/Sidali.Gmr/'
+          Color='blue-600'
+        />
+        <SocialItem
+          name='Linkedin'
+          href='https://www.linkedin.com/in/sidali-gmr-4bbba21a4/'
+          Color='blue-500'
+        />
+        <SocialItem
+          name='Email'
+          href='https://mailto:goumirisidali@hotmail.com'
+          Color='teal-500'
+        />
+        <SocialItem
+          name='Instagram'
+          href=' https://www.instagram.com/sidou_gmr/'
+          Color='orange-600'
+        />
+        <SocialItem
+          name='Messanger'
+          href=' https://www.instagram.com/sidou_gmr/'
+          Color='blue-500'
+        />
       </div>
       <div className='flex align-center justify-center mt-4 md:text-xl'>
         <p className='text-black mb-4'>
           Made by
-          <a
-            className='text-rose-600'
-            href='mailto:goumirisidali@hotmail.com'>
+          <a className='text-rose-600' href='mailto:goumirisidali@hotmail.com'>
             {' '}
             Goumiri Ali
           </a>
