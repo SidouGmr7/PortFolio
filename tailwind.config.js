603b4b2a983f09b0b1abdb27b972792e0e29d646
SidouGmr7 <goumirisidali@hotmail.com>
2023-04-08 16:09:54 +0100

    fix Card and fix some style

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,28 @@
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}",],
-  theme: {
-    extend: {
-      backgroundColor: {
-        'primary': '#27c7b7',
-        'secondary': '#68fff1'
-      },
-      textColor: {
-        'primary': '#68fff1',
-      },
-      borderColor: {
-        'custom-green': '#00FF00',
-      },
+    content: ["./src/**/*.{js,jsx,ts,tsx}"],
+    theme: {
+        extend: {
+            backgroundColor: {
+                primary: "#27c7b7",
+                secondary: "#68fff1",
+            },
+            textColor: {
+                primary: "#68fff1",
+            },
+            borderColor: {
+                "custom-green": "#00FF00",
+            },
+        },
     },
-    
-  },
-  plugins: [
-    require('tailwind-scrollbar-hide')
-  ],
+    plugins: [
+        function ({ addComponents }) {
+            addComponents({
+                ".headerTitle": {
+                    "@apply text-2xl text-primary md:text-4xl font-bold text-center pt-10 relative before:absolute before:rounded-lg before:w-20 before:h-1 before:-bottom-1 before:bg-secondary":
+                        {},
+                },
+            })
+        },
+        require("tailwind-scrollbar-hide"),
+    ],
 }
