cd3b53b1bba7cbda0ed77dbf071943fd8440b48b
SidouGmr7 <goumirisidali@hotmail.com>
2023-08-08 15:49:40 +0100

    some changes

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,12 +1,19 @@
-import React from 'react'
-import ScrollIntoView from 'react-scroll-into-view'
+import React from "react"
+import ScrollIntoView from "react-scroll-into-view"
+import { motion } from "framer-motion"
 
-const Button = ({Name, Selector}) => {
-  return (
-    <button className='bg-[#04998a] hover:bg-[#04998a] md:scale-125 md:hover:scale-150 transition text-white py-2 px-5 rounded-full'>
-      <ScrollIntoView selector={Selector}>{Name}</ScrollIntoView>
-    </button>
-  )
+const Button = ({ Name, Selector }) => {
+    return (
+        <motion.div
+            className='box'
+            whileHover={{ scale: 1.1 }}
+            transition={{ type: "spring", stiffness: 400, damping: 10 }}
+        >
+            <button className='bg-[#04998a] hover:bg-[#04998a] md:scale-125 md:hover:scale-150 transition text-white py-2 px-5 rounded-full'>
+                <ScrollIntoView selector={Selector}>{Name}</ScrollIntoView>
+            </button>
+        </motion.div>
+    )
 }
 
 export default Button
--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -1,3 +1,4 @@
+import { Grid } from "@mui/material"
 import Scroll from "../components/Scroll"
 import ServiceItem from "./components/CardItem"
 import { servicesIcons, skillsIcons } from "./components/icons"
@@ -5,18 +6,24 @@ import { servicesIcons, skillsIcons } from "./components/icons"
 const Services = () => {
     return (
         <>
-            <div className='relative w-full md:h-screen md:pt-20 pt-12' id='service'>
+            <div claussName='' id='service'>
                 <p className='headerTitle'>My Services</p>
-                <div className='grid lg:grid-cols-6 md:grid-cols-3 sm:grid-cols-2 justify-center pt-10'>
+                <div className='flex flex-wrap p-4 justify-center pt-10'>
                     {Object.keys(servicesIcons).map((name, index) => (
-                        <ServiceItem title={name} index={index} key={index} about={servicesIcons[name].about} icon={servicesIcons[name].icon} />
+                        <ServiceItem
+                            title={name}
+                            index={index}
+                            key={index}
+                            about={servicesIcons[name].about}
+                            icon={servicesIcons[name].icon}
+                        />
                     ))}
                 </div>
                 <Scroll selector='#tech' bottom='0' />
             </div>
             <div className='relative w-full md:h-screen md:pt-20 pt-12' id='tech'>
                 <p className='headerTitle'>Tech I Use</p>
-                <div className='md:grid lg:grid-cols-5 md:grid-cols-3 sm:grid-cols-2 justify-center pt-10'>
+                <div className='flex flex-wrap p-4 justify-center pt-10'>
                     {Object.keys(skillsIcons).map((name, index) => (
                         <ServiceItem title={name} index={index} key={index} icon={skillsIcons[name]} />
                     ))}
